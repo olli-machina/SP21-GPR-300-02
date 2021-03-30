5cb3798cf7776c4c32be0282507bc9e639c44b1b
Brandon LAbbe <brandon.labbe@mymail.champlain.edu>
2021-03-30 14:12:24 -0400

    Work from class 3/30

--- animal3D SDK/resource/glsl/4x/gs/00-common/drawTangentBasis_gs4x.glsl
+++ animal3D SDK/resource/glsl/4x/gs/00-common/drawTangentBasis_gs4x.glsl
@@ -42,12 +42,48 @@
 #define MAX_VERTICES 32
 
 layout (triangles) in;
+// gl_in[3]
+
+in vbVertexData {
+	mat4 vTangentBasis_view;
+	vec4 vTexcoord_atlas;
+} vVertexData[];
 
 layout (line_strip, max_vertices = MAX_VERTICES) out;
 
 out vec4 vColor;
 
+void drawWireframe()
+{
+	vColor = vec4(1.0, 0.0, 0.0, 1.0);
+
+	// get vertex info
+	// v0, v1, v2, v0
+	gl_Position = gl_in[0].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
+	EndPrimitive();
+
+	vColor = vec4(0.0, 1.0, 0.0, 1.0);
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[2].gl_Position;
+	EmitVertex();
+	EndPrimitive();
+
+	vColor = vec4(0.0, 0.0, 1.0, 1.0);
+	gl_Position = gl_in[2].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[0].gl_Position;
+	EmitVertex();
+	EndPrimitive();
+
+}
+
 void main()
 {
+
+	drawWireframe();
 	
 }
