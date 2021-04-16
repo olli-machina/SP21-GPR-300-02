3573f70d78b9b5e08821a81c0fc0f573e53dc2af
Olli Machina <olli.machina@gmail.com>
2021-04-15 22:33:06 -0600

    Finishing vertex shader

--- animal3D SDK/resource/glsl/4x/vs/04-anim/passColor_hierarchy_transform_instanced_vs4x.glsl
+++ animal3D SDK/resource/glsl/4x/vs/04-anim/passColor_hierarchy_transform_instanced_vs4x.glsl
@@ -28,7 +28,7 @@
 
 #define MAX_COLORS 24
 
-// ****TO-DO: 
+// ****DONE: 
 //	-> declare hierarchy depth info in uniform block
 //	-> use hierarchy depth to select color
 
@@ -39,6 +39,8 @@ uniform ubTransformMVP {
 };
 
 uniform vec4 uColor0[MAX_COLORS];
+
+uniform int hierarchyDepthInfo;
 
 out vec4 vColor;
 
@@ -51,7 +53,7 @@ void main()
 //	gl_Position = aPosition;
 	gl_Position = uMVP[gl_InstanceID] * aPosition;
 	
-	vColor = uColor0[gl_InstanceID];
+	vColor = uColor0[hierarchyDepthInfo];
 
 	vVertexID = gl_VertexID;
 	vInstanceID = gl_InstanceID;
